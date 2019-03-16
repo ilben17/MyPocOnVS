debc5915490fd545082816359a8af80137ca846f
Bili511 <ilies.benhaddouche@umontreal.ca>
2019-03-16 16:01:51 -0400

    rad

--- GitTest/Class1.cs
+++ GitTest/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +9,6 @@ namespace GitTest
   class Class1
   {
     public string MyFirstProp {get; set;}
+    public string MyFirstProp2 {get; set;}
   }
 }
